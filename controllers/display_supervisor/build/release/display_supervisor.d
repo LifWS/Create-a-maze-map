5c1ec381f0f9693741eca383362d46797a8099c2
LifWS <LifWS>
2021-04-11 17:08:48 +0700

    first commit

---
+++ controllers/display_supervisor/build/release/display_supervisor.d
@@ -0,0 +1,7 @@
+build/release/display_supervisor.o: display_supervisor.c \
+ C:\Program\ Files\Webots/include/controller/c/webots/display.h \
+ C:\Program\ Files\Webots/include/controller/c/webots/types.h \
+ C:\Program\ Files\Webots/include/controller/c/webots/robot.h \
+ C:\Program\ Files\Webots/include/controller/c/webots/nodes.h \
+ C:\Program\ Files\Webots/include/controller/c/webots/supervisor.h \
+ C:\Program\ Files\Webots/include/controller/c/webots/utils/system.h
